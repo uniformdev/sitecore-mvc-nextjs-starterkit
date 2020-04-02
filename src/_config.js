36f0013560750e408df0f0d4d43c9a04e6da8ea8
Alen Pelin <alen.pelin@gmail.com>
2020-04-02 18:15:50 +1100

    Add spinner for personalized components that flash

--- src/_config.js
+++ src/_config.js
@@ -6,10 +6,10 @@ const dotenv = require('dotenv');
 module.exports = function() {
     dotenv.config();
     process.env.PORT = process.env.PORT || '3000';
-    process.env.UNIFORM_API_SITENAME = process.env.UNIFORM_API_SITENAME || 'HabitatPreview'
-    process.env.UNIFORM_API_URL = process.env.UNIFORM_API_URL || 'http://habitat.dev.local'
-    process.env.UNIFORM_DATA_URL = process.env.UNIFORM_DATA_URL || 'http://habitat.dev.local'
+    process.env.UNIFORM_API_SITENAME = process.env.UNIFORM_API_SITENAME || 'HabitatPreview';
+    process.env.UNIFORM_API_URL = process.env.UNIFORM_API_URL || 'http://habitat.dev.local';
+    process.env.UNIFORM_DATA_URL = process.env.UNIFORM_DATA_URL || 'http://habitat.dev.local';
     process.env.UNIFORM_OPTIONS_MVC_SUPPORT = process.env.UNIFORM_OPTIONS_MVC_SUPPORT || 'true';
-    process.env.UNIFORM_PUBLISH_TARGET = process.env.UNIFORM_PUBLISH_TARGET || 'none',
-    process.env.UNIFORM_API_TOKEN = process.env.UNIFORM_API_TOKEN || '1234'
-}
+    process.env.UNIFORM_MODE = process.env.UNIFORM_MODE || 'preview';
+    process.env.UNIFORM_API_TOKEN = process.env.UNIFORM_API_TOKEN || '1234';
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,19 @@
 const index: any = {};
 
 // important! placeholder must be defined here to have exclusive access to index
-import { BasePlaceholder } from '@uniformdev/next';
+import { BasePlaceholder, COMPONENT_LOADER_SUFFIX } from '@uniformdev/next';
 
 import { consoleLogger } from '../utils/logging/consoleLogger';
+
+/* uncomment this to enable Spinner to be a default loader for all components with personalization
+
+import { GLOBAL_LOADER } from '@uniformdev/next';
+index[GLOBAL_LOADER] = Spinner;
+*/
+
+// enable Spinner to be a loader for PageHeaderMediaCarousel component when it is used with personalization
+import { Spinner } from './Spinner';
+index['PageHeaderMediaCarousel' + COMPONENT_LOADER_SUFFIX] = Spinner;
 
 export class Placeholder extends BasePlaceholder {
     constructor(props) {
