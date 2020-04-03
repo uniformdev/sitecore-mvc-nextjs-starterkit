1cd0993881247eaaab77a659d185d0a3e6e6470a
Alen Pelin <alen.pelin@gmail.com>
2020-04-03 22:45:30 +1100

    Update for netlify test

--- src/server.js
+++
@@ -1,7 +0,0 @@
-const _config = require('./_config');
-_config();
-
-const { server } = require('@uniformdev/next-server');
-const { serverLogger } = require('./utils/logging/serverLogger');
-
-server({ logger: serverLogger });
