8345407b43ab3f883300ae7687992f038bbd2042
Alex Shyba <as@altola.co>
2020-04-07 13:44:38 -0700

    moving static to public folder

--- src/scripts/move-static.js
+++
@@ -1,6 +0,0 @@
-const mv = require("mv");
-
-mv("out/static/fonts", "out/fonts", { mkdirp: true }, function(err) {});
-mv("out/static/styles", "out/styles", { mkdirp: true }, function(err) {});
-mv("out/static/scripts", "out/scripts", { mkdirp: true }, function(err) {});
-mv("out/static/favicon.ico", "out/favicon.ico", { mkdirp: true }, function(err) {});
