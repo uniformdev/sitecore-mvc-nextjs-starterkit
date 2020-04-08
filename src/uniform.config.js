c220ada3ca83f72123f8681f2e0ffb610e886aca
Alex Shyba <as@altola.co>
2020-04-07 17:58:24 -0700

    config: bringing back default habitat hostname

--- src/uniform.config.js
+++ src/uniform.config.js
@@ -7,8 +7,8 @@ module.exports = function() {
     dotenv.config();
     process.env.PORT = process.env.PORT || '3000';
     process.env.UNIFORM_API_SITENAME = process.env.UNIFORM_API_SITENAME || 'HabitatPreview'
-    process.env.UNIFORM_API_URL = process.env.UNIFORM_API_URL || 'http://habitat.local'
-    process.env.UNIFORM_DATA_URL = process.env.UNIFORM_DATA_URL || 'http://habitat.local'
+    process.env.UNIFORM_API_URL = process.env.UNIFORM_API_URL || 'http://habitat.dev.local'
+    process.env.UNIFORM_DATA_URL = process.env.UNIFORM_DATA_URL || 'http://habitat.dev.local'
     process.env.UNIFORM_OPTIONS_MVC_SUPPORT = process.env.UNIFORM_OPTIONS_MVC_SUPPORT || 'true';
     process.env.UNIFORM_PUBLISH_TARGET = process.env.UNIFORM_PUBLISH_TARGET || 'none',
     process.env.UNIFORM_API_TOKEN = process.env.UNIFORM_API_TOKEN || '1234'
