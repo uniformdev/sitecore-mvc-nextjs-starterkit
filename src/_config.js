58b072be7f23caace00d5e86835776bda645a15e
Alen Pelin <alen.pelin@gmail.com>
2020-04-24 01:20:09 +1000

    Update

--- src/_config.js
+++ src/_config.js
@@ -7,26 +7,16 @@ module.exports = function() {
     dotenv.config();
     process.env.PORT = 3000;
     process.env.UNIFORM_API_SITENAME = 'HabitatPreview';
-    process.env.UNIFORM_DATA_URL = 'https://habitatuniform.blob.core.windows.net/data'
-    process.env.UNIFORM_OPTIONS_MVC_SUPPORT = 'true';
-    
+    process.env.UNIFORM_OPTIONS_MVC_SUPPORT = 'true';    
     process.env.UNIFORM_OPTIONS_DEBUG = '1';
 
     //process.env.UNIFORM_PUBLISH_NEXT_BUILD_COMMAND = 'echo Compiled successfully.';
-
     process.env.UNIFORM_OPTIONS_MVC_SPA_ENABLED = 'false';
 
     process.env.UNIFORM_PUBLISH_TARGET = 'azureblob',
     process.env.AZURE_CONTAINER='$web';
-    process.env.UNIFORM_PUBLISH_AZUREBLOB_PUBLIC_URL="https://habitatuniform.z1.web.core.windows.net/"
 
     process.env.UNIFORM_EXPORT_PREFETCH_ENABLED = 'false';
     process.env.UNIFORM_MODE = 'mixed';
     process.env.UNIFORM_PUBLISH_PREFETCH_REQUEST_TIMEOUT = '10000';
-    
-
-    process.env.UNIFORM_API_URL="https://habitat-uniform-461960-single.azurewebsites.net/"
-process.env.UNIFORM_API_TOKEN="1234"
-process.env.AZURE_STORAGE_ACCOUNT="habitatuniform"
-process.env.AZURE_STORAGE_ACCESS_KEY="hDvrp2JNPD9GRjgDZefNIgWkZHjIMm8WWuRqPM73HKX1m8BA0pAdQbB/FZHyXCCMejh/VdiMETox/jiJaJ++Cw=="
 }
