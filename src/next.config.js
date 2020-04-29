688b010ff2976a8dbb5eae280c627dc15a91d32c
Alen Pelin <alen.pelin@gmail.com>
2020-04-29 14:41:09 +1000

    Try to fix failing build

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,6 @@
 const _config = require('./uniform.config');
 _config();
 
-const { uniformConfig } = require('@uniformdev/next-server');
+const { config: uniformConfig } = require('@uniformdev/next-server');
 
 module.exports = uniformConfig();
